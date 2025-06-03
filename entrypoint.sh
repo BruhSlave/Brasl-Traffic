7921a5e6106228391bc650a7c77d4f61c371eabb
BruhSlave <braslpaf@gmail.com>
2025-06-03 17:11:17 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 set -e
 
-# Запуск виртуального X-сервера
+export DISPLAY=:1
+
 Xvfb :1 -screen 0 ${SCREEN_SIZE} &
 sleep 2
 
-# Старт оконного менеджера
 fluxbox &
 sleep 1
 
-# Старт x11vnc
 x11vnc -display :1 -noxdamage -forever -shared -nopw -bg -rfbport 5900
 sleep 1
 
-# Старт веб-прокси
 /opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
 sleep 1
 
-# Запуск pygame-приложения
+# Явно задаём DISPLAY, если не передан
+export DISPLAY=${DISPLAY:-:1}
 python3 main.py
